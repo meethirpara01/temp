6beb516b352c5714da27f582f4fd6b458f4f6f57
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-11-01 20:29:33 +0530

    Daily backup - $(1-Nov-2025)

---
+++ Java Script/JS BECOME DEVELOPER/THE DOM/script.js
@@ -0,0 +1,13 @@
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<i>PARTH</i>"
+// h1.textContent = "PARTH HIRPARA";
+// h1.hidden = true;
+h1.style.color = "#3C7A9A";
+
+let a = document.querySelector("a");
+// a.href = "https://www.google.com";
+a.setAttribute("href", "https://www.google.com");
+
+
+let img = document.querySelector("img");
+img.setAttribute("src", "https://images.unsplash.com/photo-1761682719755-d745218bbdab?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=900");
